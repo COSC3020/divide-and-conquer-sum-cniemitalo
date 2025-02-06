6925baf70c08b087a3d6c0fbdfcf191804b2abe4
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-02-06 16:46:38 -0700

    implemented divideAndConquerSum

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function divideAndConquerSum(a) {
-    return a;
+    return divideConquer(a, 0, a.length - 1);
+}
+
+function divideConquer(x, low, high) {
+    if (low >= high) return x[low];
+    var third = low + Math.floor((high - low) / 3);
+    var twoThirds = low + Math.floor((2 * (high - low)) / 3);
+    var left = divideConquer(x, low, third);
+    var middle = 0; 
+    if (third != twoThirds) {
+        middle = divideConquer(x, third + 1, twoThirds);
+    }
+    var right = divideConquer(x, twoThirds + 1, high);
+    return left + middle + right;
 }
