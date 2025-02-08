8ef1a17a877b5cd871b738f79487fd6deaf81490
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-02-08 09:49:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,15 @@
 function divideAndConquerSum(a) {
-    if (a.length === 0) {
-        return 0; 
-    } else {
+    if (a.length = 0) {
+        return 0;
+    } else { 
         return divideConquer(a, 0, a.length - 1);
-    }
+    } 
 }
 
 function divideConquer(x, low, high) {
     if (low === high) {
         return x[low];
     }
-    if (high - low === 1) {
-        return x[low] + x[high]; 
-    } 
     var third = low + Math.floor((high - low + 1) / 3);
     var twoThirds = low + Math.floor((2 * (high - low + 1)) / 3);
 
