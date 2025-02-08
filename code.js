af662b809a11e885bda3bd27ceeb032660b80302
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-02-08 09:52:16 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 function divideAndConquerSum(a) {
+    //return 0 for empty array 
     if (a.length = 0) {
         return 0;
     } else { 
@@ -7,15 +8,24 @@ function divideAndConquerSum(a) {
 }
 
 function divideConquer(x, low, high) {
+    //single element 
     if (low === high) {
         return x[low];
     }
+    //two elements 
+    if (high - low === 1) {
+        return x[low] + x[high]
+    } 
+
+    //find split lengths
     var third = low + Math.floor((high - low + 1) / 3);
     var twoThirds = low + Math.floor((2 * (high - low + 1)) / 3);
 
+    //recursively split array into thirds
     var left = divideConquer(x, low, third - 1); 
     var middle = divideConquer(x, third, twoThirds - 1);
     var right = divideConquer(x, twoThirds, high);
-    
+
+    //return sum of all elements
     return left + middle + right;
 }
